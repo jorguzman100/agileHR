7c69455149920cbdd125de553b8176bb41a49f50
Jorge Guzman <jorguzman100@gmail.com>
2021-03-29 10:46:38 +0300

    Testing 3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* const FILES_TO_CACHE = [
+const FILES_TO_CACHE = [
     "/",
     "./index.html",
     "./manifest.json",
@@ -66,7 +66,7 @@ self.addEventListener("fetch", function (e) {
     );
 });
 
- */
+
 /* const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
