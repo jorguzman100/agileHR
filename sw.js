675628914f3374d6a1c947ce297dbf5fb4ac9946
Jorge Guzman <jorguzman100@gmail.com>
2021-03-29 10:25:37 +0300

    PWA updated

--- sw.js
+++ sw.js
@@ -2,11 +2,34 @@ const FILES_TO_CACHE = [
     "/",
     "./index.html",
     "./manifest.json",
+    "./sw.js",
+    "./sitemap.xml",
     "./assets/css/style.css",
     "./assets/js/script.js",
     "./assets/img/logo/logo_dice-d20_150.png",
+    "./assets/img/logo/logo_150.png",
     "./assets/img/logo/logo_192.png",
     "./assets/img/logo/logo_512.png",
+    "./assets/img/logo/maskable_icon.png",
+    "./assets/img/home_images/agileTeam.png",
+    "./assets/img/home_images/city.jpg",
+    "./assets/img/home_images/highImpactChart.jpg",
+    "./assets/img/home_images/icon_emptyImage.png",
+    "./assets/img/icons/Icon_Agile.png",
+    "./assets/img/icons/Icon_AgileHR.png",
+    "./assets/img/icons/Icon_Customer.png",
+    "./assets/img/icons/Icon_diagnosis.png",
+    "./assets/img/icons/icon_DigitalHRT.png",
+    "./assets/img/icons/icon_HRSMEs.png",
+    "./assets/img/icons/Icon_HRStrategic.png",
+    "./assets/img/icons/icon_HRT.png",
+    "./assets/img/icons/Icon_Move.png",
+    "./assets/img/icons/Icon_ROI.png",
+    "./assets/img/icons/Icon_Strategy.png",
+    "./assets/img/icons/icon_Talent.png",
+    "./assets/img/team/jorgeGuzman.jpeg",
+    "./assets/img/team/vanessaBorgonio.jpeg",
+    "./assets/img/team/manuelCordova.jpeg",
     "./assets/img/team/teamHR.jpg",
     "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
     "https://unpkg.com/aos@next/dist/aos.css",
@@ -19,11 +42,36 @@ const FILES_TO_CACHE = [
     "https://cdn.jsdelivr.net/npm/echarts@5.0.2/dist/echarts.min.js"
 ];
 
-const CACHE_NAME = "static-cache-v2";
+// install
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+// fetch
+self.addEventListener("fetch", function (e) {
+    console.log(`Intercepting fetch request for: ${e.request.url}`);
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+
+/* const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function (evt) {
+self.addEventListener("install", function (evt) {z
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Your files were pre-cached successfully!");
@@ -83,4 +131,4 @@ self.addEventListener("fetch", function (evt) {
             return response || fetch(evt.request);
         })
     );
-});
+}); */
